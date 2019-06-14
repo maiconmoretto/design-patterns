0e5fc9b015dc520161a2d6892cc3149912db51cf
maiconmoretto <maiconmorettos@gmail.com>
2019-06-14 19:45:37 -0300

    start desing pattern template Method

--- ICMS.php
+++ ICMS.php
@@ -1,15 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Maicon Moretto
- * Date: 26/07/2018
- * Time: 19:27
- */
 
 class ICMS implements  Imposto
 {
 
-    public function calcula (Orcamento $orcamento){
+    public function calcula (Orcamento $orcamento) {
+        if ($orcamento->getValor() > 500) {
+            return $orcamento->getValor() * 0.15; 
+        }
         return $orcamento->getValor() * 0.5;
     }
 }
