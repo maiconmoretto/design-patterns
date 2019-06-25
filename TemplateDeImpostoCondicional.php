dacd4e35190036e1a909fc1ba533ae2770d985ab
maiconmoretto <maiconmorettos@gmail.com>
2019-06-24 21:31:02 -0300

    Criado template de imposto condicional

---
+++ TemplateDeImpostoCondicional.php
@@ -0,0 +1,20 @@
+<?php
+
+class TemplateDeImpostoCondicional implements Imposto {
+
+    public function calcula(Orcamento $Orcamento) {
+        if ($this->deveUsarOMaximo($Orcamento)) {
+            return $this->taxacaoMaxima($Orcamento);
+        } else {
+            return $this->taxacaoMinima($Orcamento);
+        }
+    }
+
+    public abstract function deveUsarMaximo(Orcamento $Orcamento);
+
+    public abstract function taxacaoMaxima(Orcamento $Orcamento);
+
+    public abstract function taxacaoMinima(Orcamento $Orcamento);
+
+
+}
